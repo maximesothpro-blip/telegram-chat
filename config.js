8cfb88e9ebcbfb702fb6dd202eda70a29dc33aea
Maxime SOTH PRO <maximesothpro@MacBook-Air-de-Maxime.local>
2025-11-12 11:26:21 -0500

    Initial commit

---
+++ config.js
@@ -0,0 +1,4 @@
+// Configuration de l'URL du backend
+// Remplacez cette URL par l'URL de votre backend sur Hostinger une fois déployé
+// Exemple: 'https://votre-domaine.com' ou 'https://votre-sous-domaine.hostinger.com'
+window.BACKEND_API_URL = 'http://72.61.0.124:3000';
