143d7cce8a0a4ba050a394a07e67e1b6085079d8
Maxime SOTH PRO <maximesothpro@MacBook-Air-de-Maxime.local>
2025-11-12 12:21:02 -0500

    Update backend URL to Cloudflare Tunnel (HTTPS)

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // Configuration de l'URL du backend
-// Remplacez cette URL par l'URL de votre backend sur Hostinger une fois déployé
-// Exemple: 'https://votre-domaine.com' ou 'https://votre-sous-domaine.hostinger.com'
-window.BACKEND_API_URL = 'http://72.61.0.124:3000';
+// URL Cloudflare Tunnel (HTTPS) - Temporaire pour test
+window.BACKEND_API_URL = 'https://entrepreneurs-density-smoking-jefferson.trycloudflare.com';
