885af8a1dbedea591962140b25ec041a8d7ec636
Maxime SOTH PRO <maximesothpro@MacBook-Air-de-Maxime.local>
2025-11-27 09:29:52 -0500

    Update Cloudflare tunnel URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Configuration de l'URL du backend
 // URL Cloudflare Tunnel (HTTPS) - Temporaire pour test
-window.BACKEND_API_URL = 'https://implementation-stanley-forbes-antique.trycloudflare.com';
+window.BACKEND_API_URL = 'https://athletic-every-systems-appreciated.trycloudflare.com';
